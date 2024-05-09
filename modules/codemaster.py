cb76b354f83cefd862f4cee441f045d3808c3d24
kyawkaung <88552297+monsterhunters@users.noreply.github.com>
2024-05-10 03:36:26 +0630

    Update and rename launch_utils.py to codemaster.py

--- modules/codemaster.py
+++ modules/codemaster.py
@@ -462,11 +462,11 @@ def configure_for_tests():
 
 def start():
     print(f"Launching {'API server' if '--nowebui' in sys.argv else 'Web UI'} with arguments: {' '.join(sys.argv[1:])}")
-    import webui
+    import codex
     if '--nowebui' in sys.argv:
-        webui.api_only()
+        codex.api_only()
     else:
-        webui.webui()
+        codex.webui()
 
 
 def dump_sysinfo():
